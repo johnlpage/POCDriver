199bd0f15a9d2da33536e5f432a9f87861524e27
David Hows <howsdav@gmail.com>
2017-05-11 21:56:23 +1000

    Add config files

--- largescale/run-test.py
+++ largescale/run-test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import sys
 import os
 import time
 import subprocess
@@ -11,7 +12,7 @@ insert_rate = 100
 update_rate = 0
 query_rate = 0
 num_collections = 1
-total_runtime = 3600
+total_runtime = 3600 * 12
 time_to_ramp = total_runtime/2
 ramp_interval = 60
 worker_threads = 32
@@ -52,9 +53,37 @@ def launch_poc_driver():
                " -o out.csv -t " + str(worker_threads) +
                " --incrementPeriod " + str(time_to_ramp) +
                " --incrementIntvl " + str(ramp_interval)) 
+    print(command)
     java_proc = subprocess.Popen(command, shell=True)
 
+def load_from_config(filename):
+    global insert_rate, update_rate, query_rate, num_collections, total_runtime, time_to_ramp, ramp_interval, worker_threads, gross_throughput
+    with open(filename, "r") as f:
+    for line in f:
+        arr = line.split('=')
+        if arr[0] == "insert"
+            insert_rate = int(arr[1])
+        if arr[0] == "update"
+            update_rate = int(arr[1])
+        if arr[0] == "read"
+            query_rate = int(arr[1])
+        if arr[0] == "collections"
+            num_collections = int(arr[1])
+        if arr[0] == "runtime"
+            total_runtime = int(arr[1])
+        if arr[0] == "ramptime"
+            time_to_ramp = int(arr[1])
+        if arr[0] == "ramp_interval"
+            ramp_interval = int(arr[1])
+        if arr[0] == "thread"
+            worker_threads = int(arr[1])
+        if arr[0] == "throughput"
+            gross_throughput = int(arr[1])
+
 # Main
+if len(sys.argv) > 1
+    load_from_config(sys.argv[1])
+
 client = MongoClient('mongodb://localhost:27017/')
 fhandle = open(output_filename, 'a')
 fhandle.write("time,relative_time,inserts,collections,num_writes,write_latency,average_latency\n")
