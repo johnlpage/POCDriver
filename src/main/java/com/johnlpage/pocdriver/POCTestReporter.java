b4ecda8e064433df5eafe1d090b34657ab564e98
John Page <johnlpage@gmail.com>
2018-12-18 14:46:11 +0000

    Fixed missing comma in CSV output

--- src/main/java/com/johnlpage/pocdriver/POCTestReporter.java
+++ src/main/java/com/johnlpage/pocdriver/POCTestReporter.java
@@ -96,7 +96,7 @@ public class POCTestReporter implements Runnable {
                 }
                 
             }
-
+            outfile.format(",");
             
             System.out.println();
 
